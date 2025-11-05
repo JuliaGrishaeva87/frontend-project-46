eeb1f429c25172cb5fc58e688b2c36d80727c908
Julia <justjuliajust@gmail.com>
2025-11-05 10:53:18 +0300

    step 5.1

--- src/buildAdress.js
+++ src/buildAdress.js
@@ -1,6 +1,6 @@
-import path from 'path'
-import fs from 'fs'
-import { fileURLToPath } from 'url'
+import path from 'node:path'
+import fs from 'node:fs'
+import { fileURLToPath } from 'node:url'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
--- src/genDiffCal.js
+++ src/genDiffCal.js
@@ -1,7 +1,6 @@
 import path from 'node:path'
 import process from 'node:process'
 import { readFileSync } from 'node:fs'
-import _ from 'lodash'
 import { getFixturePath } from '../src/buildAdress.js'
 
 const parseObjects = (filepath) => {
@@ -12,24 +11,23 @@ const parseObjects = (filepath) => {
 const genDiff = (filepath1, filepath2) => {
   const data1 = parseObjects(getFixturePath(filepath1))
   const data2 = parseObjects(getFixturePath(filepath2))
-  const keys1 = Object.keys(data1)
-  const keys2 = Object.keys(data2)
-  const keys = _.sortBy(_.union(keys1, keys2))
+  const keys = [...new Set([...Object.keys(data1), ...Object.keys(data2)])].sort()
 
   const result = ['{\r\n']
   for (const key of keys) {
-    if (!Object.hasOwn(data1, key)) {
-      result.push(`  + ${key}: ${data2[key]}\r\n`)
+    if (data1[key] !== undefined && data2[key] !== undefined) {
+      if (data1[key] === data2[key]) {
+        result.push(`    ${key}: ${data2[key]}\r\n`)
+      }
+      else {
+        result.push(`  - ${key}: ${data1[key]}\r\n`, `  + ${key}: ${data2[key]}\r\n`)
+      }
     }
-    else if (!Object.hasOwn(data2, key)) {
+    else if (data1[key] !== undefined) {
       result.push(`  - ${key}: ${data1[key]}\r\n`)
     }
-    else if (data1[key] !== data2[key]) {
-      result.push(`  - ${key}: ${data1[key]}\r\n`)
+    else if (data2[key] !== undefined) {
       result.push(`  + ${key}: ${data2[key]}\r\n`)
-    }
-    else {
-      result.push(`    ${key}: ${data2[key]}\r\n`)
     }
   }
   result.push('}')
