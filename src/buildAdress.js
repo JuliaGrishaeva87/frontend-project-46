d7b31bacc9c84fd35fa156e75c73d8e5f17aa6b0
Julia <justjuliajust@gmail.com>
2025-11-24 12:28:09 +0300

    hex check/2

--- src/buildAdress.js
+++ src/buildAdress.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'node:url'
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
-const getFixturePath = file => path.join(dirname, '..', '__fixtures__', file)
+const getFixturePath = file => path.join(dirname, '..', '__fixtures__', file.split('/').slice(-1).join(''))
 const readFile = file => fs.readFileSync(getFixturePath(file), 'utf-8')
 
 export { getFixturePath, readFile }
